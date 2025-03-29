cf4af86c94b55cebe88c1d859b97d587bacd1541
overgodofchaos <vezonzzzzzz@gmail.com>
2025-03-29 12:56:09 +0300

    v1

---
+++ pydantic-argparse-new/backend/parser/parser.py
@@ -0,0 +1,141 @@
+from argparse import ArgumentParser
+from pydantic import BaseModel, Field
+from typing import Type, Any, get_args, get_origin, Literal
+import typing
+
+
+class Argument(BaseModel):
+    name: str = None
+    alias: str | None = None
+    required: bool | None = False
+    positional: bool = False
+    help: str | None = None
+    type: Any = None
+    default: Any = None
+    nargs: int | str | None = None
+    action: str | None = None
+    choices: list[str] | None = None
+
+    class Config:
+        validate_assignment = True
+
+    @property
+    def names(self) -> list[str]:
+        if self.alias:
+            return [self.name, self.alias]
+        return [self.name]
+
+    def parametres(self):
+        exclude = {
+            "positional",
+            "name",
+            "alias"
+        }
+
+        if self.positional:
+            exclude.add("required")
+
+            if self.required is False:
+                self.nargs = "?"
+
+        if self.action in ["store_true", "store_false"]:
+            exclude.add("type")
+            exclude.add("nargs")
+
+        for key, value in self.model_dump().items():
+            if value is None:
+                exclude.add(key)
+
+        params = self.model_dump(exclude=exclude)
+        return params
+
+
+class Extra(BaseModel):
+    positional: bool = False
+
+    class Config:
+        validate_assignment = True
+
+    # def __new__(cls, *args, **kwargs) -> dict:
+    #     obj = super().__new__(cls, *args, **kwargs)
+    #     obj.__init__()
+    #     return obj.model_dump()
+
+
+def parse(model: Type[BaseModel]) -> BaseModel:
+    parser = ArgumentParser(
+        prog="Program name",
+        description="Program description",
+        epilog="Program epilog"
+    )
+
+    fields = model.model_fields
+
+    arguments = []
+
+    for field in fields.keys():
+        field_info = fields[field]
+        argument = Argument()
+
+        argument.name = field.replace("_", "-")
+
+        if field_info.alias:
+            argument.alias = field_info.alias.replace("_", "-")
+
+        if field_info.default is not None:
+            argument.required = False
+            argument.default = field_info.default
+
+        argument.type = field_info.annotation
+
+        argument.help = field_info.description
+
+        arguments.append(argument)
+
+        if field_info.json_schema_extra:
+            extra = Extra.model_validate(field_info.json_schema_extra)
+        else:
+            extra = Extra()
+
+        if extra.positional is False:
+            argument.positional = False
+            argument.name = f"--{argument.name}"
+            if argument.alias is not None:
+                if argument.alias.startswith("-") is False:
+                    argument.alias = f"--{argument.alias}"
+        else:
+            argument.positional = True
+
+        if get_origin(field_info.annotation) is not None:
+            print(get_origin(field_info.annotation))
+            if get_origin(field_info.annotation) is list:
+                argument.type = get_args(field_info.annotation)[0]
+
+                if len(get_args(field_info.annotation)) == 1:
+                    argument.nargs = "*"
+                else:
+                    argument.nargs = len(get_args(field_info.annotation))
+            if get_origin(field_info.annotation) is typing.Literal:
+                argument.type = str
+                argument.choices = list(get_args(field_info.annotation))
+
+        if argument.type is bool:
+            if argument.default is False:
+                argument.action = "store_true"
+            elif argument.default is True:
+                argument.action = "store_false"
+            else:
+                raise IOError("Default must be configured for bool arguments")
+
+
+
+    for argument in arguments:
+        print(argument)
+        parser.add_argument(
+            *argument.names,
+            **argument.parametres()
+        )
+
+    args = parser.parse_args()
+    return model.model_validate(args.__dict__)
+
---
+++ pydantic-argparse-new/main.py
@@ -0,0 +1,1 @@
+from backend.parser import parse
